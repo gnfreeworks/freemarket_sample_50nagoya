2f221af9e109ac4362ed5feda30ef7903d933e24
Shimizu Satoshi <* 3104shimizu.info@gmail.com>
2019-08-02 19:28:52 +0900

    create large_categories model

--- db/migrate/20190802101210_create_categories.rb
+++ db/migrate/20190802101210_create_categories.rb
@@ -2,9 +2,9 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
 
-      t.integer :large_category_id, null: false
-      t.integer :medium_category_id, null: false
-      t.integer :smail_category_id, null: false
+      t.integer :large_category_id
+      t.integer :medium_category_id
+      t.integer :smail_category_id
     end
   end
 end
