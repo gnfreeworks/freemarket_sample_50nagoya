6a023b61fc0f462bc422cb573c2fa879802c3e3d
Shimizu Satoshi <* 3104shimizu.info@gmail.com>
2019-08-09 18:38:24 +0900

    setting association whth product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  belongs_to :shipping_charge
+  belongs_to :shipping_time
+  belongs_to :status
+  belongs_to :sale_charge
+  belongs_to :category
+  has_many   :product_images
+
+  # refer to active has github(https://github.com/zilkey/active_hash/blob/master/README.md)
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :area
 end
