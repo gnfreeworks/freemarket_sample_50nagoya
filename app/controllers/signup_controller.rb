4545b743226e8617b54470ab43f37202531f7d5c
nachian <natsuki.m0622@gmail.com>
2019-08-20 22:07:41 +0900

    create_view_file

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,28 @@
+class SignupController < ApplicationController
+  def step0
+      @user = User.new
+  end
+  def step1
+      @user = User.new
+  end
+
+  def step2
+      @user = User.new
+  end
+
+  def step3
+      @user = User.new
+  end
+
+  def step4
+      @user = User.new
+  end
+
+  def step5
+      @user = User.new
+  end
+
+  def done
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   root 'products#index'
 
   resources :products
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4'
+      get 'step5'
+      get 'done' 
+    end
+  end
 end
