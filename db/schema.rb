dbc1521bb48c4489364c1e6a9c245260f7ec82d7
Shimizu Satoshi <* 3104shimizu.info@gmail.com>
2019-08-24 17:16:34 +0900

    fix seeds files

--- db/schema.rb
+++ db/schema.rb
@@ -10,134 +10,134 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_09_112713) do
+ActiveRecord::Schema.define(version: 20190809112713) do
 
-  create_table "areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+  create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "banks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+  create_table "banks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "large_category_id"
-    t.bigint "medium_category_id"
-    t.bigint "smail_category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["large_category_id"], name: "index_categories_on_large_category_id"
-    t.index ["medium_category_id"], name: "index_categories_on_medium_category_id"
-    t.index ["smail_category_id"], name: "index_categories_on_smail_category_id"
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "large_category_id"
+    t.integer  "medium_category_id"
+    t.integer  "smail_category_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["large_category_id"], name: "index_categories_on_large_category_id", using: :btree
+    t.index ["medium_category_id"], name: "index_categories_on_medium_category_id", using: :btree
+    t.index ["smail_category_id"], name: "index_categories_on_smail_category_id", using: :btree
   end
 
-  create_table "large_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "medium_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+  create_table "large_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "product_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "product_id", null: false
-    t.text "url", null: false
+  create_table "medium_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description", null: false
-    t.integer "price", null: false
-    t.integer "profit", null: false
-    t.integer "area_id", null: false
-    t.string "brand"
-    t.integer "size_id"
-    t.bigint "sale_charge_id"
-    t.bigint "status_id"
-    t.bigint "category_id"
-    t.bigint "shipping_charge_id"
-    t.bigint "shipping_time_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
-    t.index ["sale_charge_id"], name: "index_products_on_sale_charge_id"
-    t.index ["shipping_charge_id"], name: "index_products_on_shipping_charge_id"
-    t.index ["shipping_time_id"], name: "index_products_on_shipping_time_id"
-    t.index ["status_id"], name: "index_products_on_status_id"
+  create_table "product_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "product_id",               null: false
+    t.text     "url",        limit: 65535, null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
-  create_table "sale_charges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.float "rate", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                             null: false
+    t.text     "description",        limit: 65535, null: false
+    t.integer  "price",                            null: false
+    t.integer  "profit",                           null: false
+    t.integer  "area_id",                          null: false
+    t.string   "brand"
+    t.integer  "size_id"
+    t.integer  "sale_charge_id"
+    t.integer  "status_id"
+    t.integer  "category_id"
+    t.integer  "shipping_charge_id"
+    t.integer  "shipping_time_id"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.index ["category_id"], name: "index_products_on_category_id", using: :btree
+    t.index ["sale_charge_id"], name: "index_products_on_sale_charge_id", using: :btree
+    t.index ["shipping_charge_id"], name: "index_products_on_shipping_charge_id", using: :btree
+    t.index ["shipping_time_id"], name: "index_products_on_shipping_time_id", using: :btree
+    t.index ["status_id"], name: "index_products_on_status_id", using: :btree
   end
 
-  create_table "shipping_charges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "sale_charges", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.float    "rate",       limit: 24, null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
-  create_table "shipping_times", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+  create_table "shipping_charges", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "sizes_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "category_id"
-    t.bigint "size_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_sizes_categories_on_category_id"
-    t.index ["size_id"], name: "index_sizes_categories_on_size_id"
-  end
-
-  create_table "smail_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+  create_table "shipping_times", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+  create_table "sizes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "kananame", null: false
-    t.integer "birthdaydate", null: false
-    t.string "nickname"
-    t.string "maildaddress", null: false
-    t.string "password", null: false
-    t.text "profiletext"
-    t.integer "authenticphonenumber", null: false
+  create_table "sizes_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "category_id"
+    t.integer  "size_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_sizes_categories_on_category_id", using: :btree
+    t.index ["size_id"], name: "index_sizes_categories_on_size_id", using: :btree
+  end
+
+  create_table "smail_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                               null: false
+    t.string   "kananame",                           null: false
+    t.integer  "birthdaydate",                       null: false
+    t.string   "nickname"
+    t.string   "maildaddress",                       null: false
+    t.string   "password",                           null: false
+    t.text     "profiletext",          limit: 65535
+    t.integer  "authenticphonenumber",               null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
   end
 
   add_foreign_key "categories", "large_categories"
