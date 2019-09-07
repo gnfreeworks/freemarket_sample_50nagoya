372a60f41199641b7cbde7e788d1bffe333be0af
Shimizu Satoshi <* 3104shimizu.info@gmail.com>
2019-09-07 15:31:45 +0900

    add transaction tab files

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -2,11 +2,14 @@ window.addEventListener("DOMContentLoaded", function() {
   let tabs = document.getElementsByClassName("tab_item");
   tabsAry = Array.prototype.slice.call(tabs);
 
+  let transtabs = document.getElementsByClassName("transaction_tab");
+  transtabsAry = Array.prototype.slice.call(transtabs);
+
   tabsAry.forEach(function(value){
     value.addEventListener('click', function(e){
       //デフォルトアクション無効化
       e.preventDefault();
-      
+
       //選択されたタブ以外を非表示: active, show class削除
       document.getElementsByClassName("active")[0].classList.remove("active");
       document.getElementsByClassName("mypage-item-list")[0].classList.remove("show");
@@ -22,6 +25,23 @@ window.addEventListener("DOMContentLoaded", function() {
       }else{
         document.getElementsByClassName("mypage-nav-number")[0].classList.add("hidden-large");
       }
+    });
+  });
+
+  transtabsAry.forEach(function(value){
+    value.addEventListener('click', function(e){
+      //デフォルトアクション無効化
+      e.preventDefault();
+
+      //選択されたタブ以外を非表示: active, show class削除
+      document.getElementsByClassName("active")[1].classList.remove("active");
+      document.getElementsByClassName("show")[1].classList.remove("show");
+      
+      // //選択されたタブを表示:active, show class追加
+      const index = transtabsAry.indexOf(this);
+      document.getElementsByClassName("transaction_tab")[index].classList.add("active");
+      document.getElementsByClassName("transaction-list")[index].classList.add("show");
+
     });
   });
 });
