fb31eea3665a430490723872f6ac036875e1327e
Shimizu Satoshi <* 3104shimizu.info@gmail.com>
2019-09-14 18:37:49 +0900

    update hover and controller function

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,6 +1,9 @@
 class MypageController < ApplicationController
 
   def index
+    @user = User.find(1)
+    @evaluation_count = @user.buyer_evaluations.count
+    @products_count = ProductsStatus.where(buyer_id: @user.id).count
   end
   
 end
