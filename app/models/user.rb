6d8bd294b6303c5f90ebe5ef318a7393848a151e
nachian <natsuki.m0622@gmail.com>
2019-09-17 19:42:27 +0900

    select_box birthday

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ApplicationRecord
-  def name
-    firstname + lastname
-  end
-  def kananame
-    firstkananame + lastkananame
-  end
-
-  def set_extra_information
-    {:name => name, :kananame => kananame}
+  def date
   end
 end
