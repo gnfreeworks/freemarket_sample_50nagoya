f93e8703b90eb187d63414afc81c7482e95ce85d
Satoshi Shimizu <50259919+Satoshi-Shimizu@users.noreply.github.com>
2019-09-17 22:41:13 +0900

    Revert "Create mypage"

--- app/controllers/mypage_controller.rb
+++
@@ -1,9 +0,0 @@
-class MypageController < ApplicationController
-
-  def index
-    @user = User.find(1)
-    @evaluation_count = @user.buyer_evaluations.count
-    @products_count = ProductsStatus.where(buyer_id: @user.id).count
-  end
-  
-end
--- app/models/good.rb
+++
@@ -1,4 +0,0 @@
-class Good < ApplicationRecord
-  belongs_to :products_status
-  belongs_to :user
-end
