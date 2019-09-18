f32d1f8ac5a76d6782a5c7cbe5de21984409bbd5
Shimizu Satoshi <* 3104shimizu.info@gmail.com>
2019-09-18 23:38:15 +0900

    add function "profile" and "nickname"

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -5,5 +5,26 @@ class MypageController < ApplicationController
     @evaluation_count = @user.buyer_evaluations.count
     @products_count = ProductsStatus.where(buyer_id: @user.id).count
   end
+
+  def profile
+    @user = User.find(1)  
+
+  end
+
+  def profileupdate
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_profile)
+      render 'profile'
+    else
+      render 'profile'
+    end
+
+  end
   
+  private
+  def user_profile
+  params.permit(:nickname, :profiletext)
+
+  end
+
 end
