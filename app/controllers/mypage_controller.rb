95f7e8b774eaefa04a315dabafa23c3395df1670
Shimizu Satoshi <* 3104shimizu.info@gmail.com>
2019-09-25 01:15:09 +0900

    update credit card save function

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -12,10 +12,19 @@ class MypageController < ApplicationController
   
   def cardcreate
     @user = User.find(1)
+    @month = ['--', '1',  '2',  '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+    @year =  {'--': '--', '19': '2019', '20': '2020', '21': '2021', '22': '2022', '23': '2023', '24': '2024', '25': '2025', '26': '2026', '27': '2027', '28': '2028', '29': '2029'}
     @creditcard = PaymentMethod.new()
   end
 
   def cardadd
+    @creditcard = PaymentMethod.new(credit_param)
+    if @creditcard.save
+      redirect_to cardcreate_user_mypage_index_path, notice:'クレジットカードを追加しました!'
+    else
+      redirect_to cardcreate_user_mypage_index_path, notice:'もう一度入力して下さい'
+    end
+
   end
 
   def profile
@@ -40,7 +49,7 @@ class MypageController < ApplicationController
   end
 
   def credit_param
-    params.permit(:nickname, :profiletext)
+    params.require(:payment_method).permit(:card_number, :expiration_month, :expiration_year, :secrity_code).merge(user_id: params[:user_id])
   end
 
 end
