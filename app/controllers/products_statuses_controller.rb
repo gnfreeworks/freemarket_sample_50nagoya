f252badb0ad0c5168fb6e57e76c082f6d6a7ab57
Genki Nishiyama <genki.nishiyama@gmail.com>
2019-10-14 11:41:17 +0900

    Added the ability to display information from the database

--- app/controllers/products_statuses_controller.rb
+++ app/controllers/products_statuses_controller.rb
@@ -50,6 +50,28 @@ class ProductsStatusesController < ApplicationController
   end
 
   def buy
+    # 製品情報
+    @product_status = ProductsStatus.find(1)
+
+    # ユーザー情報
+    @user = User.find(@product_status.buyer_id)
+
+    # 製品名
+    @product_name = @product_status.product.name
+
+    # 価格
+    @price = @product_status.product.price.to_s(:delimited)
+
+    # 配送先 - 清水さんへ確認
+    #ユーザー登録・ログインが実装されてから設定
+
+    # クレジットカード番号
+    @card_number = @user.payment_method.card_number
+
+    # クレジットカード期限（month/year)
+    expiration_month = @user.payment_method.expiration_month
+    expiration_year = @user.payment_method.expiration_year
+    @expiration = "#{format("%02d",expiration_month)} / #{format("%02d",expiration_year)}"
 
   end
   
