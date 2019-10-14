c8ccc591a1c040e4f27d2b67e0a69e093f72b0da
Genki Nishiyama <genki.nishiyama@gmail.com>
2019-10-15 00:33:09 +0900

    Add html and css of view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'products_statuses' => 'products_statuses#index'
   get 'products_buy' => 'products_statuses#buy'
+  get 'sell' => 'sell#create'
 
   resources :users, only: :index do
     resources :mypage, only: :index do
