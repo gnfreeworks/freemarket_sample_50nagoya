207ea40b6e567b9bfedfb2abaa28b8d19135715d
Shimizu Satoshi <* 3104shimizu.info@gmail.com>
2019-12-02 21:46:22 +0900

    delete comment

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,22 +3,16 @@ class SearchController < ApplicationController
   before_action :set_categories, only: :index
   
   def index
-    #レディース商品
     @ladies_products = ProductsStatus.where("category_parent_id = 1 and buyer_id IS NULL").limit(10)
-    
-    #メンズ商品
+
     @mens_products = ProductsStatus.where("category_parent_id = 219 and buyer_id IS NULL").limit(10)
-
-    # 家電・スマホ・カメラ カテゴリ
+    
     @goods_products = ProductsStatus.where("category_parent_id = 983 and buyer_id IS NULL").limit(10)
-
-    # おもちゃ・ホビー・グッズ カテゴリ
+    
     @toy_products = ProductsStatus.where("category_parent_id = 748 and buyer_id IS NULL").limit(10)
-
-    # シャネル ブランド
+    
     @chanel_products = ProductsStatus.where("brand_id = 2446 and buyer_id IS NULL").limit(10)
-
-    # ルイヴィトン ブランド
+    
     @lv_products = ProductsStatus.where("brand_id = 6154 and buyer_id IS NULL").limit(10)
   end
 
