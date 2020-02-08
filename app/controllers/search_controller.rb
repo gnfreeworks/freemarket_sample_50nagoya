5fb471f87e1d6bbc8c50d40f9d76e660a942519d
Genki Nishiyama <genki.nishiyama@gmail.com>
2020-02-08 19:50:27 +0900

    Create product list display function
    商品一覧表示の機能を実装。

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,23 +4,23 @@ class SearchController < ApplicationController
   
   def index
     # レディース
-    @ladies_products = ProductsStatus.where("category_parent_id = 1 and buyer_id IS NULL").limit(10)
+    @ladies_products = ProductsStatus.where("category_parent_id = 1 and buyer_id IS NULL").order(created_at: :desc).limit(10)
 
     # メンズ
-    @mens_products = ProductsStatus.where("category_parent_id = 219 and buyer_id IS NULL").limit(10)
+    @mens_products = ProductsStatus.where("category_parent_id = 219 and buyer_id IS NULL").order(created_at: :desc).limit(10)
     
     # 家電・スマホ・カメラ
-    @goods_products = ProductsStatus.where("category_parent_id = 983 and buyer_id IS NULL").limit(10)
+    @goods_products = ProductsStatus.where("category_parent_id = 983 and buyer_id IS NULL").order(created_at: :desc).limit(10)
     
     # おもちゃ・ホビー・グッズ
-    @toy_products = ProductsStatus.where("category_parent_id = 748 and buyer_id IS NULL").limit(10)
+    @toy_products = ProductsStatus.where("category_parent_id = 748 and buyer_id IS NULL").order(created_at: :desc).limit(10)
     
     # ブランド
       # シャネル
-    @chanel_products = ProductsStatus.where("brand_id = 2446 and buyer_id IS NULL").limit(10)
+    @chanel_products = ProductsStatus.where("brand_id = 2446 and buyer_id IS NULL").order(created_at: :desc).limit(10)
     
     # ヴィトン
-    @lv_products = ProductsStatus.where("brand_id = 6154 and buyer_id IS NULL").limit(10)
+    @lv_products = ProductsStatus.where("brand_id = 6154 and buyer_id IS NULL").order(created_at: :desc).limit(10)
   end
 
 end
