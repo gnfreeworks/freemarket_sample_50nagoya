306e8b3762adf00f5ba9bfda59284349ad3c9254
Genki Nishiyama <genki.nishiyama@gmail.com>
2020-02-24 03:31:48 +0900

    Update products_status.rb

--- app/models/products_status.rb
+++ app/models/products_status.rb
@@ -1,5 +1,5 @@
 class ProductsStatus < ApplicationRecord
-  belongs_to  :product
+  belongs_to  :product, dependent: :destroy
   belongs_to  :buyer, class_name: 'User', :foreign_key => 'buyer_id'
   belongs_to  :seller, class_name: 'User', :foreign_key => 'seller_id'
   has_one     :canseling_product
